4dfa96f023524c91a6ff34d2ae2dc850cdf7ec25
tommenguyen0602 <tommenguyen0602@gmail.com>
2022-11-17 20:08:34 +0700

    test replace with ftp

--- index.php
+++ index.php
@@ -8,30 +8,6 @@
     
 </head>
 <body>
-    <script src="chatbox.js"></script>
-    
-    <div id="navigation">
-        <?php include 'navigation.php';?>
-    </div>
-    <div class="container-fluid">
-        <?php include 'carousel.php';?>
-    </div>
-    <div class = "container" id="random-products">
-        <?php 
-        include 'random-products.php';
-        ?>
-        <div id="chat-box">
-    </div>
-    </div>
-    
-    <div id="footer">
-        <?php include 'footer.php';?>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-    crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-    crossorigin="anonymous"></script>
+    <p>test replacable</p>
 </body>
 </html>
