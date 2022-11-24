07ddbcbd8ec8c3008ee3b02b2973dea0f4ced454
fallenazura003 <78076005+fallenazura003@users.noreply.github.com>
2022-11-24 03:08:51 +0000

    update shipping_policy.php

--- views/shipping_policy.php
+++ views/shipping_policy.php
@@ -2,20 +2,20 @@
 <html lang="vi">
 <head>
     <title>SE13|Home</title>
-    <?php include 'head.php'; ?>    
+    <?php include 'head.html'; ?>    
     <link rel="stylesheet" href="return-policy.css">
 </head>
 <body>
     
 <div id = "nav_menu">
-        <?php include 'nav_menu.php'
+        <?php include 'nav_menu.html'
         ?>
     </div> 
     <div class = "container-fluid" id = "background"  style="padding-top: 80px">
         <img class = "img-fluid" src="/images/shipping-policy.png" alt="">
     </div>
     <div id="footer">
-        <?php include 'footer.php';?>
+        <?php include 'footer.html';?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
     integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
