c839d63f5da811185085d34ab3e08caf9e01bc39
fallenazura003 <78076005+fallenazura003@users.noreply.github.com>
2022-11-29 10:28:25 +0700

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -1,4 +1,9 @@
 <?php
 include 'models/single_product.php';
 echo $result;
+$loader = new \Twig\Loader\FilesystemLoader('views');
+$twig = new \Twig\Environment($loader);
+echo $twig->render('product_details.html', array(
+    'name' => 'egoist'
+));
 ?>
