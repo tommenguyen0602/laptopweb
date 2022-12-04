d6089f6380310f94d1c04abf94984e348d319904
tommenguyen0602 <tommenguyen0602@gmail.com>
2022-12-04 15:34:41 +0700

    update product_details views

--- models/single_product.php
+++ models/single_product.php
@@ -10,7 +10,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
 $sql = "SELECT * FROM products WHERE productId = 4";
 $result = $conn->query($sql);
 $product = $result->fetch_assoc();
